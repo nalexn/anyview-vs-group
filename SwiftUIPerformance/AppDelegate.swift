5ae7cb3b540b6fea7aa6290cf58de9b7e2dd0891
Alexey Naumov <a.naumov91@gmail.com>
2019-11-29 19:02:44 +0300

    Create project

---
+++ SwiftUIPerformance/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  SwiftUIPerformance
+//
+//  Created by Alexey Naumov on 29.11.2019.
+//  Copyright © 2019 Alexey Naumov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions
+        launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
---
+++ SwiftUIPerformance/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  SwiftUIPerformance
+//
+//  Created by Alexey Naumov on 29.11.2019.
+//  Copyright © 2019 Alexey Naumov. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        let contentView = ContentView()
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: contentView)
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+    }
+}
